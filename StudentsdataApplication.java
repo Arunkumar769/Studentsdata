e128d2df36e8d93acff75d0b4c0f2bd1cf3bacc2
Arunkumar769 <157477410+Arunkumar769@users.noreply.github.com>
2025-03-21 11:52:20 +0530

    Add files via upload

---
+++ StudentsdataApplication.java
@@ -0,0 +1,13 @@
+package com.mru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsdataApplication.class, args);
+	}
+
+}
---
+++ students.java
@@ -0,0 +1,7 @@
+package com.mru.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface students extends JpaRepository<students, Integer> {
+
+}
